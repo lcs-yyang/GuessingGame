3206054fe151184039310249b4926043f88db7f4
Yang, Yuxin <yyang@lcs-mba3025.local>
2018-01-19 15:01:05 -0500

    The app can run one round now.

--- GuessingGame/ViewController.swift
+++ GuessingGame/ViewController.swift
@@ -9,17 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Outlets
+    @IBOutlet weak var guessedNumber: UITextField!
+    @IBOutlet weak var textFieldGuess: UITextField!
+    @IBOutlet weak var labelMessage: UILabel!
+    //MARK: Properties (variables)
+    var game = GuessingGame()
+    
+    
+    
+    //MARK: Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Let's see what's the random number actually is
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: Actions
+    @IBAction func checkGuess(_ sender: Any) {
+        
+        print("A guess was made.")
+        print(guessedNumber.text)
+        
+        //First we need to unwrap the optional text from
+        // the text field
+        // inputGiven is a String (non-opotional) - Guaranteed
+        // to contain a value (it is not nill)
+        guard let inputGiven = guessedNumber.text else {
+           return
+        }
+        
+        guard let guessedNumber = Int(inputGiven) else {
+            //We couldn't make an integer
+            // e.g.: Somebody typed "five" instead of 5
+            return
+        }
+        
+        
+        //Print the actual number that was given
+        print(guessedNumber)
+        
+        //Print out the number that needs to be guessed
+        print("The number to be guessed is:")
+        print(game.numberToGuess)
+        
+        // Compare the guess provided to the number to be guessed
+        // if-else if-else
+        labelMessage.text = game.compareNumbers(guessMade: guessedNumber)
+    }
+    
 }
+
+
+
 
---
+++ GuessingGame/guessingGame.swift
@@ -0,0 +1,37 @@
+//
+//  guessingGame.swift
+//  GuessingGame
+//
+//  Created by Yang, Yuxin on 2018-01-17.
+//  Copyright © 2018 Yang, Jennifer. All rights reserved.
+//
+
+import Foundation
+struct GuessingGame {
+    
+    
+    // Property
+    var numberToGuess : Int
+    
+    // Initializer
+    init() {
+        numberToGuess = Int(arc4random_uniform(501)) //Generate a number between 0 and 500 (but not 501)
+    }
+    
+    // Methods
+    func compareNumbers(guessMade: Int) -> String {
+        
+        // Compare the guess made to the secret number
+        if guessMade == numberToGuess {
+            return "You got it!"
+        } else if guessMade > numberToGuess{
+            return "Guess lower."
+        } else {
+            return "Guess higher."
+        }
+        
+    }
+    
+
+}
+
